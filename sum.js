2b03fc2704491a531504b8e9b7448fbe60ae5bf6
shaquibkhan <connectshaquib@gmail.com>
2017-04-29 17:23:54 +0530

    commit to master
    commit to master

---
+++ sum.js
@@ -0,0 +1,18 @@
+
+var num = 0;
+// for loop is used..
+for (i=1;i<=1000;i++)
+{
+	num += i ; 						// num is storing the sum of all natural numbers upto 1000...
+}
+console.log("SUM IS : " +num+ " for loop");
+
+var num1 = 0;
+// while is used..
+var j=1;
+while (j<=1000)      //condition ..
+{
+	num1 += j ;
+	j++;
+}
+console.log("SUM IS : " +num1+ " while loop");
